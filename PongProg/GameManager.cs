d82121775c2b25f1d5973f3e5a45eee5a9e56fa2
cameronheine <70171437+cameronheine@users.noreply.github.com>
2021-02-07 20:13:23 -0800

    Add files via upload

---
+++ PongProg/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Player1")]
+    public GameObject Player1Paddle;
+    public GameObject Player1Goal;
+
+    [Header("Player2")]
+    public GameObject Player2Paddle;
+    public GameObject Player2Goal;
+
+    [Header("ScoreBox")]
+    public GameObject Player1Text;
+    public GameObject Player2Text;
+
+    private int player1Score;
+    private int player2Score;
+
+    private void ResetPosition()
+    {
+        ball.GetComponent<Ball>().Reset();
+        Player1Paddle.GetComponent<Paddle>().Reset(); 
+        Player2Paddle.GetComponent<Paddle>().Reset();
+    }
+
+    public void Player1Scored()
+    {
+        player1Score++;
+        Player1Text.GetComponent<TextMeshProUGUI>().text = player1Score.ToString();
+        ResetPosition();
+    }
+    public void Player2Scored()
+    {
+        player2Score++;
+        Player2Text.GetComponent<TextMeshProUGUI>().text = player2Score.ToString();
+        ResetPosition();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+           Application.Quit();
+        }
+        
+    } 
+}
